9c3091354c38f38653fd82c3b775330391f8c23b
YM W <949426508@qq.com>
2018-05-29 20:00:43 +0800

    Revert "Revert "更改界面排布""
    This reverts commit 2a3ad815edffdf30f1d57c4467b42edcb85dcfad.

--- PINS/Activity1.cs
+++ PINS/Activity1.cs
@@ -15,11 +15,13 @@ namespace PINS
     [Activity(Label = "Activity1")]
     public class Activity1 : Activity
     {
+        /*
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
         }
+        */
     }
 }
